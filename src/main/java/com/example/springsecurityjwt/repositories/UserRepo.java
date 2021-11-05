b9049bc27d4f216db2ed2cb600dd3cdff7ac1610
Zaid <iamzaidsheikh@gmail.com>
2021-11-05 16:51:21 +0530

    build: Add UserRepo.java interface

---
+++ src/main/java/com/example/springsecurityjwt/repositories/UserRepo.java
@@ -0,0 +1,12 @@
+package com.example.springsecurityjwt.repositories;
+
+import com.example.springsecurityjwt.models.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+
+    //Spring Data JPA understands the function by how we define its name
+    User findByUsername(String username);
+
+}
